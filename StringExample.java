7c2a0437d6338a472223eef15d62b5b1423d78f1
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-08-22 17:34:30 -0500

    Delete StringExample.java

--- StringExample.java
+++
@@ -1,8 +0,0 @@
-package holamundo;
-public class StringExample {
-    public static void main(String[]args){
-        String dospalabras = "Hola Mundo :v";
-        System.out.println(dospalabras);
-    }
-    
-}
