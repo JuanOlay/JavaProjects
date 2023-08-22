01401546678159c33013775a0c1618226f692eb8
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-08-22 17:35:12 -0500

    Add files via upload

---
+++ PrimerosCodigos/Variables1.java
@@ -0,0 +1,9 @@
+package holamundo;
+public class Variables1 {
+    public static void main(String[] args) {
+        String nombre = "Ryan Gosling";
+        int fechadenacimiento = 1980;
+        System.out.println(nombre);
+        System.out.println(fechadenacimiento);
+}
+}
