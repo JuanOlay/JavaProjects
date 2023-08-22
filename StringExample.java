570fe8832963ebbdebe7398d57dd8f02fde55746
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-08-22 17:33:59 -0500

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,8 @@
+package holamundo;
+public class StringExample {
+    public static void main(String[]args){
+        String dospalabras = "Hola Mundo :v";
+        System.out.println(dospalabras);
+    }
+    
+}
