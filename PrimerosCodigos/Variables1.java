79b3213dd972f13bd918767e30f9f9db146b81d3
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-08-27 22:51:49 -0500

    Delete Variables1.java

--- PrimerosCodigos/Variables1.java
+++
@@ -1,9 +0,0 @@
-package holamundo;
-public class Variables1 {
-    public static void main(String[] args) {
-        String nombre = "Ryan Gosling";
-        int fechadenacimiento = 1980;
-        System.out.println(nombre);
-        System.out.println(fechadenacimiento);
-}
-}
