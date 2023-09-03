adcd6f98679ec38aa7b27298d8d1d6793cf89509
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-09-03 00:55:32 -0500

    Add files via upload

---
+++ loops/ForInfinitiveLoopExample.java
@@ -0,0 +1,10 @@
+public class ForInfinitiveLoopExample {  
+public static void main(String[] args) {  
+
+	System.out.println("Oprima control + c para detener el loop");
+ 
+    for(;;){  
+        System.out.println("infinitive loop");  
+    }  
+}  
+}  
---
+++ loops/WhileExample2.java
@@ -0,0 +1,8 @@
+public class WhileExample2 {    
+public static void main(String[] args) {   
+ 	System.out.println("Oprima control`+ c para salir del loop");
+    while(true){    
+        System.out.println("infinitive while loop");    
+    }    
+}    
+}    
