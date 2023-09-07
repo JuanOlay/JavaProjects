40d0e78d7e8e788dd8c65a12cf73356345e4833d
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-09-07 17:06:55 -0500

    Add files via upload

---
+++ encapsulacion y acceso/banco/CheckingAccount.java
@@ -0,0 +1,12 @@
+public class CheckingAccount{
+
+	public String name;
+	public int balance;
+
+	public CheckingAccount(String imputName, int imputBalance){
+
+	name = imputName;
+	balance=imputBalance;
+
+	}
+} 
