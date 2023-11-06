f883c886e56b1d8420427603419e66c15b4158b7
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-11-06 13:37:56 -0500

    Create Welcome.java

---
+++ Segundo Corte /JOptionPane/Welcome.java
@@ -0,0 +1,12 @@
+package welcome;
+
+import javax.swing.JOptionPane;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null,"¡welcome\nto\nthe Java world!");
+       System.exit(0);
+    }
+    
+}
