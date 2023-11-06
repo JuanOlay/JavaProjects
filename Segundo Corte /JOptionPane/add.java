02e516fc735f63831080c68b5d03468453604a8d
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-11-06 13:28:19 -0500

    Add.java

--- Segundo Corte /JOptionPane/add.java
+++ Segundo Corte /JOptionPane/add.java
@@ -1,24 +1,22 @@
+package add;
 import javax.swing.JOptionPane;
+public class Add {
 
-public class JavaApplication20 {
     public static void main(String[] args) {
-        String value1;
-        String value2;
-        int result, number1 , number2;
-        
-        value1 = JOptionPane.showInputDialog("Ingrese el primer numero");
-        value2 = JOptionPane.showInputDialog("Ingrese el primer numero");
-        
-        number1 = Integer.parseInt(value1);
-        number2 = Integer.parseInt(value2);
-        
-        result = number1 + number2;
-        
-       
-        JOptionPane.showMessageDialog(null,"Resultado es: " + result,"Resultado",JOptionPane.PLAIN_MESSAGE);
-        
-        System.exit(0);
+      String value1;
+      String value2;
     
-    }
+    int result, number1, number2;
     
+    value1=JOptionPane.showInputDialog("Enter first number");
+    value2=JOptionPane.showInputDialog("Enter second number");
+    
+    number1=Integer.parseInt(value1);
+    number2=Integer.parseInt(value2);
+     
+    result=number1+number2;
+    
+    JOptionPane.showMessageDialog(null,"Result is:"+ result, "Result",JOptionPane.PLAIN_MESSAGE);
+    System.exit(0);
+       }
 }
