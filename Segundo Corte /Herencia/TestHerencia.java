9302dfc2f6def5235c6b85d380ccb4b9d2d25868
JuanOlay <142933841+JuanOlay@users.noreply.github.com>
2023-11-14 10:12:33 -0500

    Update TestHerencia.java

--- Segundo Corte /Herencia/TestHerencia.java
+++ Segundo Corte /Herencia/TestHerencia.java
@@ -1,18 +1,57 @@
-package Test;
+package test;
 
 import inhiretance.*;
 
 import java.util.Date;
-
-
 public class TestHerencia {
-    public static void main (String[]args) {
+    public static void main(String[] args) {
         
-              
-        Employee empleado1 = new Employee("Alex", 3000); 
-        System.out.println(empleado1);
-        Employee empleado2 = new Employee("Reol", 4000); 
-        System.out.println(empleado2);
-    }
-}
+        Person persona = new Person("Alex", 'M', 25, "Cusco");
 
+        imprimir(persona);
+
+        Employee empleado1 = new Employee(3000,"Gutierrez");
+
+        imprimir(empleado1);
+        
+        var fecha = new Date();
+        Client cliente = new Client(fecha,true,"Juan",'M',25,"Santo Tomás");
+             
+        imprimir(cliente);
+        determinarTipo(persona);
+        determinarTipo(empleado1);
+        determinarTipo(cliente);
+        
+        String miNombre = "Henry";
+        determinarTipo(miNombre);
+        
+
+    Person p1 = new Person("Henry", 'M', 45, "Bogotá");
+    Person p2 = new Person("Henry", 'M', 45, "Bogotá");
+    
+    System.out.println(p1 == p2);
+    System.out.println("Hola" == "Hola");
+    System.out.println(p1.equals(p2));
+    
+    
+    }        
+    
+    public static void imprimir(Person personaje) {
+        System.out.println(personaje.obtenerDetalle());
+    }
+    
+    
+           
+    public static void determinarTipo(Object objeto){
+        if(objeto instanceof Employee){
+            System.out.println("Es de Tipo Empleado");
+        }else if (objeto instanceof Client){
+            System.out.println("Es de tipo Cliente");
+        }else if (objeto instanceof Person){
+            System.out.println("Es de Tipo Persona");
+        }else if (objeto instanceof Object){
+            System.out.println("Es de tipo Object");
+        }
+    }
+
+}
